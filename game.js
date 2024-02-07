8feafa550d1a5648072ac639bb2054175db03073
Jade Wilson <jadegithub24@protomail.com>
2024-02-07 22:07:52 +1300

    added miner

--- game.js
+++ game.js
@@ -4,3 +4,17 @@ function btnCoin() {
     coin++;
     document.getElementById("addCoins").innerHTML = coin;
 }
+let miner = 0;
+let minerUpgrade = 10;
+
+function btnMiner() {
+    if (coin >= minerUpgrade) {
+        miner++;
+        coin = coin - minerUpgrade;
+
+        minerUpgrade = Math.round(minerUpgrade * 1.1);
+        document.getElementById("addCoins").innerHTML = coin;
+        document.getElementById("addMiner").innerHTML = miner;
+        document.getElementById("minerCost").innerHTML = minerUpgrade;
+    }
+}
